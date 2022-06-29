a8807994e5e0c58051e9c9f78118765d3f6c7fbc
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-06-29 18:59:03 -0400

    added randomness functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,5 +32,39 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// 3. prompt user for password criteria
+var promptUser = function() {
+  
+  //ask how long password will be
+  characterCount = window.prompt("Please enter yourt password length (8-128)")
+  while (characterCount < characterMin || characterCount > characterMax) {
+    characterCount = window.prompt("Sorry, please enter your password length (8-128)")
+  };
+
+  //ask if they will include lowercase characters  
+  includeLower = window.confirm("Do you want to include lowercase characters?");
+  if (includeLower) {
+    totalCharacters = totalCharacters + lowerCharacters;
+  };
+
+  //ask if they will include uppercase characters 
+  includeUpper = window.confirm("Do you want to include uppercase characters?");
+  if (includeUpper) {
+    totalCharacters = totalCharacters + upperCharacters;
+  };
+
+  //ask if they will include numeric characters
+  includeNum = window.confirm("Do you want to include numbers?");
+  if (includeNum) {
+    totalCharacters = totalCharacters + numericCharacters;
+  };
+
+  //ask if they will include special characters 
+  includeSpecial = window.confirm("Do you want to include special characters?");
+  if (includeSpecial) {
+    totalCharacters = totalCharacters + specialCharacters;
+  };
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
